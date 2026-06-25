6c567958e88eab8d942769f2acefda00d2c2f59a
Jonathan Alele <jonathanalele@gmail.com>
2026-06-25 17:53:18 +0200

    sample experiment
    euclidean metric

--- examples/experiment_0.rs
+++ examples/experiment_0.rs
@@ -1,18 +1,29 @@
-use keamogetswe::space::CubeSpace;
+use keamogetswe::export::{GeodesicOutput, Metadata, PathEntry};
 use keamogetswe::metric::EuclideanMetric;
+use keamogetswe::space::CubeSpace;
 
 fn main() {
     let _space = CubeSpace;
     let _metric = EuclideanMetric;
-    let _start = [0.2, 0.3, 0.4];
-    let _end = [0.8, 0.6, 0.7];
+    let _start = [0.0,0.0,0.3];
+    let _end = [1.0, 0.2, 0.8];
     let _resolution = 128;
 
-    // TODO: Once the solver is implemented:
-    // let path = keamogetswe::solver::solve(&space, &metric, start, end, resolution);
-    // let output = GeodesicOutput { ... };
-    // std::fs::write("viewer/sample_data/experiment_0.json", output.to_json().unwrap());
+    let path = keamogetswe::solver::solve(&_space, &_metric, _start, _end, _resolution);
+    let output = GeodesicOutput {
+        metadata: Metadata {
+            name: String::from("Experiment 0: Euclidean validation"),
+            metric: String::from("EuclideanMetric"),
+            resolution: _resolution,
+        },
+        paths: vec![PathEntry {
+            label: String::from("computed"),
+            points: path.points,
+        }],
+    };
 
-    println!("Experiment 0: solver not yet implemented");
-    println!("Using synthetic sample data in viewer/sample_data/");
+    let json = output.to_json().unwrap();
+    std::fs::write("viewer/sample_data/experiment_0.json", json).unwrap();
+
+    println!("Experiment 0 complete. Total cost: {:.4}", path.total_cost);
 }
